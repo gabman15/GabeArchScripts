0c690cf277627aeba3952155458c496c19fa6ba8
gabman15 <gabrielmcninch@gmail.com>
2024-05-25 22:53:33 -0400

    major polybar reorganization

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
 killall -q polybar
+killall -q polybar-msg
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	MONITOR=$m polybar --reload bar_top &
-	MONITOR=$m polybar --reload bar_bottom &
-done
-else
-	polybar --reload bar_top &
-	polybar --reload bar_bottom &
+HOSTNAME=`hostname`
+export HOSTCONFDIR=$HOSTNAME
+if [ $HOSTNAME = 'gensokyo' ]; then
+    MONITOR="DP-2" polybar --reload bar_top_left &
+    MONITOR="DP-2" polybar --reload bar_bottom_left &
+    MONITOR="DVI-D-0" polybar --reload bar_top_right &
+    MONITOR="DVI-D-0" polybar --reload bar_bottom_right &
+else # 1 screen
+    if type "xrandr"; then
+	    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	        MONITOR=$m polybar --reload bar_top &
+	        MONITOR=$m polybar --reload bar_bottom &
+        done
+    else
+	    polybar --reload bar_top &
+	    polybar --reload bar_bottom &
+    fi
 fi
 
 echo "Polybar launched..."
